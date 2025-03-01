785c6352d2bfa54d9d8c1ec9d76003c5da4ef9ba
AnastasiaDeka <99515576+AnastasiaDeka@users.noreply.github.com>
2025-03-01 20:56:51 +0300

    сервер

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -70,16 +70,13 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'foodgram_db',
-        'USER': 'foodgram_user',
-        'PASSWORD': 'foodgram_password',
-        'HOST': 'db',
-        #'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': os.getenv('POSTGRES_DB', 'kittygram_db'),
+        'USER': os.getenv('POSTGRES_USER', 'kittygram_user'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'kittygram_password'),
+        'HOST': os.getenv('DB_HOST', 'db'),
+        'PORT': os.getenv('DB_PORT', 5432)
     }
 }
-
-
 
 
 AUTH_PASSWORD_VALIDATORS = [
