6503ce9dcb752473d5664c2f6b3d66034eda8ec9
AnastasiaDeka <99515576+AnastasiaDeka@users.noreply.github.com>
2025-04-13 15:04:50 +0300

    ссылки

--- backend/api/views.py
+++ backend/api/views.py
@@ -338,4 +338,4 @@ class RecipeViewSet(viewsets.ModelViewSet):
         """Перенаправление по короткой ссылке на рецепт."""
         recipe = get_object_or_404(Recipe, short_link=short_link)
 
-        return HttpResponseRedirect(f'/recipes/{recipe.pk}/')
+        return HttpResponseRedirect(f'/recipes/{recipe.short_link}/')
